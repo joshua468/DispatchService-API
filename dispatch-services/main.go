77ba0062ce61a4688e5667fe3fd58c5c10ed1100
Adegbesan Joshua Temitope <112911898+joshua468@users.noreply.github.com>
2023-11-06 20:28:07 +0100

    Add files via upload

---
+++ dispatch-services/main.go
@@ -0,0 +1,45 @@
+// main.go
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type DispatchOrder struct {
+	ID          int    `json:"id"`
+	Customer    string `json:"customer"`
+	Destination string `json:"destination"`
+	Status      string `json:"status"`
+}
+
+var orders []DispatchOrder
+
+func main() {
+	router := gin.Default()
+
+	// Create a new dispatch order
+	router.POST("/dispatch", func(c *gin.Context) {
+		var newOrder DispatchOrder
+		if err := c.BindJSON(&newOrder); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Simulate order ID generation (you might want to use a database)
+		newOrder.ID = len(orders) + 1
+		newOrder.Status = "Created"
+
+		orders = append(orders, newOrder)
+
+		c.JSON(http.StatusCreated, newOrder)
+	})
+
+	// Get all dispatch orders
+	router.GET("/dispatch", func(c *gin.Context) {
+		c.JSON(http.StatusOK, orders)
+	})
+
+	router.Run(":8080")
+}
